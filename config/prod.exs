6b35164ea9e997640e8bb0e9e69e7645a7d51c05
jundalisay <jundalisay@yahoo.com>
2021-12-10 09:01:03 +0800

    update

--- config/prod.exs
+++ config/prod.exs
@@ -1,7 +1,8 @@
 import Config
 
 config :govern, GovernWeb.Endpoint,
-  url: [host: "govern.herokuapp.com", port: 443],
+  # url: [host: "govern.herokuapp.com", port: 443],
+  url: [scheme: "https", host: "govern.herokuapp.com", port: 443], force_ssl: [rewrite_on: [:x_forwarded_proto]], 
   cache_static_manifest: "priv/static/cache_manifest.json"
 
 # Do not print debug messages in production
